ea9ed830bcacbfa41b46a700edf42df85602bd72
Luc Cary <luc.cary@gmail.com>
2020-05-19 09:02:13 -0400

    Auto-format source files and add minipro git submodule

--- src/make_rom.py
+++ src/make_rom.py
@@ -1,4 +1,13 @@
-rom = bytearray([0xea] * 32768)
+"""
+Usage: python make_rom.py
+
+Inspect output: hexdump -C rom.bin
+
+Write to EEPROM: minipro -p AT28C256 -w rom.bin
+"""
+
+
+rom = bytearray([0xEA] * 32768)
 
 with open("rom.bin", "wb") as out_file:
     out_file.write(rom)
